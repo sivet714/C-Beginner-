71b9797f42018afcbe813bd8b326d3371ec54cdf
Stephen Sivetts <stephensivetts@stephens-macbook-pro.local>
2016-04-24 12:49:34 -0600

    Initial Commit

---
+++ C++Beginner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Beginner
+//
+//  Created by Stephen Sivetts on 4/24/16.
+//  Copyright © 2016 Stephen Sivetts. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
